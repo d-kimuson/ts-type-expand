dd34e09b402b968a27b13fbb8b60f71b2478a628
d-kimsuon <d-kimuson@gmail.com>
2022-04-23 04:33:38 +0900

    style(*): fix deploy script & add beta project

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -12,8 +12,8 @@ if [[ $current_version = $new_version ]]; then
 fi
 
 # deploy
-sed -i -e "s/"$current_version"/"$new_version"/g" package.json
+sed -i -e "s/"$current_version"/"$new_version"/g" package.json  # for mac
 \rm package.json-e
 git add package.json && git commit -m "$new_version release"
 git tag -a $new_version -m "$new_version release"
-vsce publish
+vsce publish --yarn
