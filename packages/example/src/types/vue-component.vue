83d40fbedc4a362efe2fbc92ec59fe6f0a3015c6
d-kimsuon <dev.kimuson@gmail.com>
2024-05-16 20:14:09 +0900

    chore(example): add sample for vue component to example package

---
+++ packages/example/src/types/vue-component.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="count++">{{ count }}</button>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const count = ref(0)
+
+    // expose to template and other options API hooks
+    return {
+      count
+    }
+  },
+
+  mounted() {}
+}
+</script>
